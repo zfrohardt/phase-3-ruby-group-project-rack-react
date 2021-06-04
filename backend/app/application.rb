eab3e0ccfaefed49e8b7385ac4f62723fd900cf8
Zach Frohardt <zfrohardt@gmail.com>
2021-06-03 21:50:00 -0700

    added second endpoint

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -1,12 +1,22 @@
-class Application
+require 'json'
+require 'pry'
 
+class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/test/) 
-      return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
+    # if req.path.match(/test/) 
+    #   return [200, { 'Content-Type' => 'application/json' }, [ {:message => "test response!"}.to_json ]]
 
+    if req.path.match(/spells/)
+      spells = JSON.parse(Spell.all.to_json)
+      spells.each do |spell|
+        spell["classes"] = Spell.find(spell["id"]).characters.collect{ |c| c.name }
+      end
+      return [200, { 'Content-Type' => 'application/json' }, [ spells.to_json ]]
+    elsif req.path.match(/classes/)
+      return [200, { 'Content-Type' => 'application/json' }, [ Character.all.collect{ |c| c.name }.to_json ]]
     else
       resp.write "Path Not Found"
 
